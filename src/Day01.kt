c12b00a98a4eaa358e0cea6abfef8484f8b99499
Serafin Delle-Rose <serafin.delle.rose@ista.com>
2021-12-03 11:54:08 +0100

    solve day01 part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,5 +1,5 @@
 fun main() {
-    fun sonarDataIncreases(next: String, prev: String) = next.toInt() > prev.toInt()
+    fun sonarDataIncreases(next: String, prev: String) = next.toLong() > prev.toLong()
 
     fun part1(input: List<String>): Int {
         var count = 0
@@ -20,15 +20,33 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val iterator = input.iterator()
+        val slidingWindowEntries = mutableListOf<String>()
+
+        for ((index, value) in iterator.withIndex()) {
+            val indexSecond = index + 1
+            val seconWindowEntry = input.getOrNull(indexSecond)
+
+            val indexThird = index + 2
+            val thirdWindowEntry = input.getOrNull(indexThird)
+
+            if (seconWindowEntry != null && thirdWindowEntry != null) {
+                val sumOfSlidingWindow = value.toLong() + seconWindowEntry.toLong() + thirdWindowEntry.toLong()
+                slidingWindowEntries.add(sumOfSlidingWindow.toString())
+            }
+        }
+
+        return part1(slidingWindowEntries)
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(testInput.size == 2000)
-    val resultCount = part1(testInput)
-    println("Increases times: " + resultCount)
-    check(resultCount == 1559)
+    val resultCountPart1 = part1(testInput)
+    println("Part 1 increases times: " + resultCountPart1)
+    check(resultCountPart1 == 1559)
 
-    println(part2(testInput))
+    check(testInput.size == 2000)
+    val resultCountPart2 = part2(testInput)
+    println("Part2 increases times: " + resultCountPart2)
+    check(resultCountPart2 == 1600)
 }
